db94a13bf585373d5dc820f89fd61e09e53d1984
lwk <l>
2017-04-27 21:51:05 +0800

    react-f

---
+++ rmanytime/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <h1>nihao, world!</h1>,
+  document.getElementById('root2')
+);
+//ReactDom.render(p1,p2) 把p1放入p2中
+//JSX可以使用html标签
